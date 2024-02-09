83e37c799a8000e35a251817131289f7c7474782
Jaeseong <easeon78@gmail.com>
2024-02-09 15:56:51 +0900

    0209 study

--- 6form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ 6form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,11 +1,10 @@
 package hello.itemservice.domain.item;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
@@ -32,6 +31,10 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
