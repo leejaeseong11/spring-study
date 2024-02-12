3469699347799b489db2a301114c9033ff9d848f
Jaeseong <easeon78@gmail.com>
2024-02-13 02:59:00 +0900

    0213 study

--- 8validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 8validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,9 +9,16 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
+// ObjectError 사용 방법 중 하나이지만 JDK 14 이후에는 사용 불가능
+// @ScriptAssert(
+//        lang = "javascript",
+//        script = "_this.price * _this.quantity >= 10000",
+//        message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
     private Long id;
-    @NotBlank private String itemName;
+
+    @NotBlank(message = "공백X")
+    private String itemName;
 
     @NotNull
     @Range(min = 1000, max = 1000000)
