f0004f4492d111058cc7a68b486979c99dc710c0
Jaeseong <easeon78@gmail.com>
2024-02-18 02:42:23 +0900

    0218 study

--- 14jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ 14jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,8 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.jupiter.api.Test;
@@ -28,6 +30,20 @@ public class ConnectionTest {
         // DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource data = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(data);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 풀에서 커넥션 생성 시간 대기
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
