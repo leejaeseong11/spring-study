6518bd2584f3a9b102ff787ab67948dc5e3ce35f
Jaeseong <easeon78@gmail.com>
2024-04-05 16:11:51 +0900

    aspect aop

--- 18proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 18proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
-import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
@@ -22,7 +17,8 @@ import org.springframework.context.annotation.Import;
 // @Import(ProxyFactoryConfigV1.class)
 // @Import(ProxyFactoryConfigV2.class)
 // @Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+// @Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(
         scanBasePackages = "hello.proxy.app.v3") // 컴포넌트 스캔 범위를 지정하여 원하는 패키지의 것만 빈으로 등록
 public class ProxyApplication {
