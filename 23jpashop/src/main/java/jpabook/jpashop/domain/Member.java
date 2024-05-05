76027cd84b593bf99b0f3687144565d7528da2c4
Jaeseong <easeon78@gmail.com>
2024-05-05 15:40:42 +0900

    api develop basic

--- 23jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 23jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +18,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String name;
+    @NotEmpty private String name;
 
     @Embedded private Address address;
 
--- README.md
+++ README.md
@@ -44,7 +44,7 @@
 - jpashop
 - jpql
 
-9. [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발](https://www.inflearn.com/course/스프링부트-JPA-활용-1)
+9. [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발](https://www.inflearn.com/course/스프링부트-JPA-활용-1) / [실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화](https://www.inflearn.com/course/스프링부트-JPA-API개발-성능최적화)
 - jpashop
 
 ### 스프링 입문
